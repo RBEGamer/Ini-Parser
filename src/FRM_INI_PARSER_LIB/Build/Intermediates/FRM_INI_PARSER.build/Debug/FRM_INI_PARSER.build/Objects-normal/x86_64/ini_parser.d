97b387ef95b1a2558a3abcd8c65d1958cfb378eb
RBEGamer <marcel.ochsendorf@gmail.com>
2016-11-05 01:36:47 +0100

    added lib

---
+++ src/FRM_INI_PARSER_LIB/Build/Intermediates/FRM_INI_PARSER.build/Debug/FRM_INI_PARSER.build/Objects-normal/x86_64/ini_parser.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/marcelochsendorf/Desktop/ini_lib/FRM_INI_PARSER/ini_parser.cpp \
+  /Users/marcelochsendorf/Desktop/ini_lib/FRM_INI_PARSER/ini_parser.hpp
---
+++ src/FRM_INI_PARSER_TEST/FRM_INI_PARSER_TEST/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FRM_INI_PARSER_TEST
+//
+//  Created by Marcel Ochsendorf on 05.11.16.
+//  Copyright © 2016 Marcel Ochsendorf. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
