7c2c10dfcee31989c697eee4c72ea829faa8ee45
RBEGamer <marcel.ochsendorf@gmail.com>
2016-11-05 13:02:49 +0100

    add sample code

--- src/FRM_INI_PARSER_TEST/FRM_INI_PARSER_TEST/main.cpp
+++ src/FRM_INI_PARSER_TEST/FRM_INI_PARSER_TEST/main.cpp
@@ -6,10 +6,38 @@
 //  Copyright © 2016 Marcel Ochsendorf. All rights reserved.
 //
 
-#include <iostream>
+/*
+ TO RUN THIS TEST CREATE A config.ini file with this content in your buid dir:
+ 
+ ;Window resolution settings
+ [window]
+ window_title=WINDOW_NAME
+ ;file parsing settings
+ [directories]
+ asset_dir=Assets
+ 
+ */
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include "frm_ini_parser.hpp"
+int ini_parser_unit_test(int argc, const char * argv[]) {
+    
+    FRN::ini_parser* parser = new FRN::ini_parser();
+    parser->load_ini_file("./config.ini");
+    
+    std::string* test =parser->get_value("window", "window_title");
+    std::string* test1 =parser->get_value("directories", "asset_dir");
+    
+    delete  parser;
+    parser = nullptr;
+    //CHECK RESULT
+    if(*test != "WINDOW_NAME" || *test1 != "Assets"){
+        return  -1;
+    }
+    
     return 0;
 }
